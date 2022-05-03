561e0558903d254b23570a183ecbbe852fe1b97d
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-05-04 00:21:57 +0900

    Move to AC & Clean up

---
+++ Assets/ActivateCode/CH/Scripts/Monster/TestMonster.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActiveCode.CH
+{
+    public class TestMonster : Monster
+    {
+        protected void Update()
+        {
+            base.Update();
+
+            Debug.Log($"HP: {this.curHp} / Defense: {this.defense} / Damage: {this.damage}");
+        }
+
+        protected override void OnDie()
+        {
+            Debug.Log($"{this.gameObject.name} 는 주금");
+
+            Destroy(this.gameObject);
+        }
+        protected override void OnHit(GameObject weaponObject)
+        {
+            Debug.Log($"{this.gameObject.name} 는 {weaponObject.name} 한테 뚜드려 맞음");
+
+            // [TODO] get weapon damage
+            this.curHp -= 10;
+        }
+    }
+}
